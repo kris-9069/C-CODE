d54c43de1c655cf4573a83e6f556f707a32a7332
kris-9069 <3090617573@qq.com>
2021-02-03 20:09:42 +0800

    多字符从两端向中间汇聚

---
+++ peter_02_03/peter_02_03/peter_02.c
@@ -0,0 +1,28 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Windows.h>
+int main()
+{
+	char arr1[]="hellow peter!!How are you?";
+	char arr2[]="$$$$$$$$$$$$$$$$$$$$$$$$$$";
+	int ret=0,i=0,j=ret;
+	int left=0,right= ret;;
+	ret=strlen(arr1)-1;
+	printf("%s\n",arr2);
+	system("cls");
+	for(left=0,right=ret;left<=right;left++,right--)
+	
+	{
+		i=left;
+		j=right;
+		arr2[left]=arr1[i];
+	    arr2[right]=arr1[j];
+		printf("%s\n",arr2);
+		system("cls");
+		Sleep(1000);
+	}
+	printf("%s\n",arr2);
+	return 0;
+}
