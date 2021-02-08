f397b1ebc304271bbf8093fa681988c71c1c05de
kris-9069 <3090617573@qq.com>
2021-02-08 10:00:09 +0800

    递归顺序打印

---
+++ peter_02_15/peter_02_15/peter.c
@@ -0,0 +1,19 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+void print(int a)
+{
+	if(a>9)
+	{
+		int n=a/10;
+	    print(n);
+	}
+	printf("%d ",a%10);
+}
+int main()
+{
+	int a=0;
+	printf("«Î ‰»Î ˝◊÷:");
+	scanf("%d",&a);
+	print(a);
+	return 0;
+}
