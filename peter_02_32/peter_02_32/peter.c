3bed8537638698a89f17e927d0e90c16d87f160a
kris-9069 <3090617573@qq.com>
2021-02-23 10:42:34 +0800

    递归n的k次方

---
+++ peter_02_32/peter_02_32/peter.c
@@ -0,0 +1,22 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+double test(int n,int k)
+{
+	if(k>0)
+		return n*test(n,k-1);
+	else if(k==0)
+		return 1.0;
+	else
+		return (1.0/(test(n,-k)));
+}
+int main()
+{
+	int n=0,k=0;
+	double ret=0;
+	scanf("%d%d",&n,&k);
+	ret=test(n,k);
+	printf("%dµÄ%d´Î·½:>%lf\n",n,k,ret);
+	system("pause");
+	return 0;
+}
